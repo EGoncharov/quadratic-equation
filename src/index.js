8e39c37d7a060a65d87df8e103845ec65397c388
Eugene Goncharov <eugen.goncharov@gmail.com>
2018-09-30 18:21:07 +0300

    Task done (third correction of the decision)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 module.exports = function solveEquation(equation) {
-
-	var a, b, c, d, x1, x2, arr;
+	
+	var a, b, c, d, num1, num2, arr;
 
 	equation = equation.replace(/\s/g, "").replace(/\^2/, "").split(/\*x/);
 
@@ -8,13 +8,14 @@ module.exports = function solveEquation(equation) {
 	b = +equation[1];
 	c = +equation[2];
 	d = b * b -4 * a * c;
-	x1 = Math.round((- b - Math.sqrt(d)) / (2 * a));
-	x2 = Math.round((- b - Math.sqrt(d)) / (2 * a));
-	arr = [x1,x2];
-
+	num1 = Math.round((- b - Math.sqrt(d)) / (2 * a));
+	num2 = Math.round((- b + Math.sqrt(d)) / (2 * a));
+ 	arr = [num1, num2];
+	
 	arr.sort(function(a,b){
 		return a-b;
-	})
-
+	});
+	
 	return arr;
+
 }
